2410bb51f2d6d7093d0f9abc97c5bba3662bf5db
razumolla <rajuahmed9344@gmail.com>
2022-03-26 13:22:41 +0600

    cart design compleate

--- src/components/SingleBag/SingleBag.js
+++ src/components/SingleBag/SingleBag.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './SingleBag.css';
+import { BsCartCheck } from 'react-icons/bs';
 
 const SingleBag = (props) => {
     console.log(props.bag);
     const { name, img, price } = props.bag;
     return (
-        <div>
-          
+        <div className='container'>
+            <div className='image'>
+                <img height={240} width={250} src={img} alt="" />
+            </div>
+            <div className='bag-info'>
+                <h3> {name}</h3>
+                <p>Price: ${price}</p>
+            </div>
+
+            <button className='cart-btn'>
+                <p>Add to Cart</p>
+                <BsCartCheck className='icon' />
+            </button>
+
         </div>
     );
 };
